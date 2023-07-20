e2bf3f2faa777651f13595d05cee41fef3dfd3ba
Иван Вдовин <98765309+UbahWin@users.noreply.github.com>
2023-07-20 22:13:59 +0300

    Time: 46 ms (69.76%), Space: 14.9 MB (6.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var dict: [Int : [Int]] = [:]
+        
+        for i in 0..<nums.count {
+            if let value = dict[nums[i]] {
+                print(value)
+                print(nums[i])
+                if value[0] == nums[i] {
+                    return [value[1], i]
+                }
+            }
+            dict[target - nums[i]] = [target - nums[i], i]
+        }
+        return []
+    }
+}
