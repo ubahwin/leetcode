ed7b92d0f56d1ac94e4aca157971abbaf529def1
Иван Вдовин <98765309+UbahWin@users.noreply.github.com>
2023-07-20 13:11:44 +0300

    Time: 112 ms (78.54%), Space: 14.7 MB (49.23%) - LeetHub

--- 0136-single-number/0136-single-number.swift
+++ 0136-single-number/0136-single-number.swift
@@ -1,16 +1,9 @@
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        let sortedNums = nums.sorted()
-        var current = 0
-        
-        for i in 0..<sortedNums.count {
-            
-            if i % 2 == 0 {
-                current = sortedNums[i]
-            } else if current != sortedNums[i] {
-                return current
-            }
+        var result = 0
+        for i in nums {
+            result ^= i
         }
-        return current
+        return result
     }
 }
