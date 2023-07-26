fb9e92df1962dc549d371a58ee6b481eb81df1f9
Иван Вдовин <98765309+UbahWin@users.noreply.github.com>
2023-07-26 18:40:37 +0300

    Time: 130 ms (23.76%), Space: 14.9 MB (99.01%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.swift
@@ -0,0 +1,21 @@
+
+class NumArray {
+    init(_ nums: [Int]) {
+        self.nums = nums
+    }
+    var nums: [Int]
+    
+    func sumRange(_ left: Int, _ right: Int) -> Int {
+        var res = 0
+        for i in left...right {
+            res += nums[i]
+        }
+        return res
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * let obj = NumArray(nums)
+ * let ret_1: Int = obj.sumRange(left, right)
+ */
