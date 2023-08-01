c2a72f632226c0a98f818cbf5bab7560fff3fa78
Иван Вдовин <98765309+UbahWin@users.noreply.github.com>
2023-08-01 17:33:04 +0300

    Time: 86 ms (90.32%), Space: 14.5 MB (25.81%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+        if root1 == nil, root2 == nil  {
+            return nil
+        }
+        
+        let res = TreeNode((root1?.val ?? 0) + (root2?.val ?? 0)) 
+        res.left = mergeTrees(root1?.left, root2?.left)
+        res.right = mergeTrees(root1?.right, root2?.right)
+
+        return res
+    }
+}
