61040e8a957db505a6acb33f3e619eb0773b12e2
Иван Вдовин <98765309+UbahWin@users.noreply.github.com>
2023-08-19 16:27:56 +0300

    Time: 700 ms (19.95%), Space: 13.9 MB (91.84%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        var iS = 0, iT = 0
+        let nT = t.count - 1, nS = s.count - 1
+
+        while iT <= nT + 1 {
+            if iS > nS {
+                return true
+            }
+            if iT > nT {
+                return false
+            }
+            var sChar = s[s.index(s.startIndex, offsetBy: iS)]
+            var tChar = t[t.index(t.startIndex, offsetBy: iT)]
+    //        print(sChar, tChar, iS, iT)
+            if sChar != tChar {
+                iT += 1
+            } else {
+                iT += 1
+                iS += 1
+            }
+        }
+        return false
+    }
+}
