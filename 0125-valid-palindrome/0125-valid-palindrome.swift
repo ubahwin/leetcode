4750b1cd83a02c26d000534c3e06af835cc9e82e
Иван Вдовин <98765309+ubahwin@users.noreply.github.com>
2023-09-11 15:12:00 +0300

    Time: 13 ms (71.69%), Space: 15.8 MB (43.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.swift
+++ 0125-valid-palindrome/0125-valid-palindrome.swift
@@ -2,14 +2,15 @@ class Solution {
     func isPalindrome(_ s: String) -> Bool {
         let s = Array(s)
         var i = 0, j = s.count - 1
+        let symbols = [" ", ",", ":", ".", "@", "#", "_", "[", "]", "{", "}", "\\", "'", "\"", "-", "?", ";", "!", "(", ")", "`"]
         
         while i < j {
-            while (s[i] == " " || s[i] == "," || s[i] == ":" || s[i] == "." || s[i] == "@" || s[i] == "#" || s[i] == "_" || s[i] == "[" || s[i] == "]" || s[i] == "{" || s[i] == "}" || s[i] == "\\" || s[i] == "'" || s[i] == "\"" || s[i] == "-" || s[i] == "?" || s[i] == ";" || s[i] == "!" || s[i] == "(" || s[i] == ")" || s[i] == "`") {
+            while symbols.contains(String(s[i])) {
                 i += 1
                 if i == s.count - 1 { return true }
             }
 
-            while (s[j] == " " || s[j] == "," || s[j] == ":" || s[j] == "." || s[j] == "@" || s[j] == "#" || s[j] == "_" || s[j] == "[" || s[j] == "]" || s[j] == "{" || s[j] == "}" || s[j] == "\\" || s[j] == "'" || s[j] == "\"" || s[j] == "-" || s[j] == "?" || s[j] == ";" || s[j] == "!" || s[j] == "(" || s[j] == ")" || s[j] == "`") {
+            while symbols.contains(String(s[j])) {
                 j -= 1
                 if j == 0 { return true }
             }
