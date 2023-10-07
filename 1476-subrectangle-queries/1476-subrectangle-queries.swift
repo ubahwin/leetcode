d68b0e62f5c08e89d8f1ba5afbb3540d5946e417
Иван Вдовин <98765309+ubahwin@users.noreply.github.com>
2023-10-07 11:08:56 +0300

    Time: 434 ms (63.16%), Space: 15.1 MB (47.37%) - LeetHub

---
+++ 1476-subrectangle-queries/1476-subrectangle-queries.swift
@@ -0,0 +1,27 @@
+
+class SubrectangleQueries {
+    private var _rectangle = [[Int]]()
+    
+    init(_ rectangle: [[Int]]) {
+        _rectangle = rectangle
+    }
+    
+    func updateSubrectangle(_ row1: Int, _ col1: Int, _ row2: Int, _ col2: Int, _ newValue: Int) {
+        for row in row1...row2 {
+            for col in col1...col2 {
+                _rectangle[row][col] = newValue
+            }
+        }
+    }
+    
+    func getValue(_ row: Int, _ col: Int) -> Int {
+        return _rectangle[row][col]
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * let obj = SubrectangleQueries(rectangle)
+ * obj.updateSubrectangle(row1, col1, row2, col2, newValue)
+ * let ret_2: Int = obj.getValue(row, col)
+ */
