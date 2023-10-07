bca014fa3e4507a3ed56424bdbc7f34ca7983393
Иван Вдовин <98765309+ubahwin@users.noreply.github.com>
2023-10-07 10:28:46 +0300

    Time: 4 ms (28.21%), Space: 13.6 MB (41.03%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.swift
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.swift
@@ -8,8 +8,10 @@ class Solution {
             number /= 10
         }
         
+        // 6242 -> 2246
         arr.sort()
         
+        // 2246 -> 24 + 26 = min sum
         var num1 = Int(String(arr[0]) + String(arr[2]))!
         var num2 = Int(String(arr[1]) + String(arr[3]))!
 
