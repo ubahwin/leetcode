08ebd7416e4942e8c37a286767b9288a56b8d1a6
Иван Вдовин <98765309+ubahwin@users.noreply.github.com>
2023-10-08 12:01:13 +0300

    Time: 77 ms (78.66%), Space: 14.4 MB (33.76%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.swift
@@ -0,0 +1,33 @@
+class Solution {
+    func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        let len = flowerbed.count
+        if flowerbed == [1] || flowerbed == [1,1] || flowerbed == [0,1] || flowerbed == [1,0] {
+            if n == 0 { return true }
+            return false
+        } else if flowerbed == [0,0] || flowerbed == [0] {
+            if n == 1 || n == 0 { return true }
+            return false
+        }
+        
+        var flowerbed = flowerbed
+        var count = 0
+        
+        if flowerbed[0] == 0 && flowerbed[1] == 0 {
+            flowerbed[0] = 1
+            count += 1
+        }
+
+        for i in 1..<len - 1 {
+            if flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0 {
+                flowerbed[i] = 1
+                count += 1
+            }
+        }
+
+        if flowerbed[len - 1] == 0 && flowerbed[len - 2] == 0 {
+            count += 1
+        }
+        
+        return count >= n
+    }
+}
