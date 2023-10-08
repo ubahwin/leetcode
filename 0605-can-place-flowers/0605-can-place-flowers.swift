b4e65421a4508930f579d18c949621895af7ae5e
Иван Вдовин <98765309+ubahwin@users.noreply.github.com>
2023-10-08 12:05:10 +0300

    Time: 79 ms (64.65%), Space: 14.1 MB (92.68%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.swift
+++ 0605-can-place-flowers/0605-can-place-flowers.swift
@@ -12,18 +12,31 @@ class Solution {
         var flowerbed = flowerbed
         var count = 0
         
+        /*
+            [ 0, 0, 0, ... ]
+              ^  ^
+        */
         if flowerbed[0] == 0 && flowerbed[1] == 0 {
             flowerbed[0] = 1
             count += 1
         }
-
+        
+        /*
+            [ ..., 0, 0, 0, 0, 0, ... ]
+                      ^  ^  ^
+                     i-1 i i+1
+        */
         for i in 1..<len - 1 {
             if flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0 {
                 flowerbed[i] = 1
                 count += 1
             }
         }
-
+        
+        /*
+            [ ..., 0, 0, 0 ]
+                      ^  ^
+        */
         if flowerbed[len - 1] == 0 && flowerbed[len - 2] == 0 {
             count += 1
         }
